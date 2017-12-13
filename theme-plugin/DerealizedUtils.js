02cdf9da6bd3f7ed3136b5ef88c090aeb9032d29
Prodiginus <Prodiginus@yahoo.com>
2017-12-12 17:42:04 -0800

    Update DerealizedUtils.js

--- theme-plugin/DerealizedUtils.js
+++ theme-plugin/DerealizedUtils.js
@@ -1,16 +1,23 @@
-let derealizedUtils = {}
+let derealized = {}
 
 const cssURL = `https://rawgit.com/P-o-u-t/Pouts-Theme/master/custom-discord-reborn/Pouts-Theme.source.css`
 
-derealizedUtils.addCSS = () => {
-        let head = document.getElementsByTagName('head')[0]
-        let link = document.createElement('link')
-        link.type = 'text/css'
-        link.rel  = 'stylesheet'
-        link.href = cssURL
-        head.appendChild(link)
+derealized.start = () => {
+    addCSS()
 }
-derealizedUtils.removeCSS = () => {
-        let cssLink = document.querySelectorAll(`link[href='${cssURL}']`)[0]
-        head.removeChild(cssLink)
+derealized.end = () => {
+    removeCSS()
+}
+
+addCSS = () => {
+    let head = document.getElementsByTagName('head')[0]
+    let link = document.createElement('link')
+    link.type = 'text/css'
+    link.rel  = 'stylesheet'
+    link.href = cssURL
+    head.appendChild(link)
+}
+removeCSS = () => {
+    let cssLink = document.querySelectorAll(`link[href='${cssURL}']`)[0]
+    head.removeChild(cssLink)
 }
